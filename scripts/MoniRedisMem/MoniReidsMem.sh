1d373f40c4cfda4c4f497b726d349c39e77edba1
wangchunyan1 <wangchunyan1@letv.com>
2016-09-07 16:31:00 +0800

    send redis monitor mail by cmdb interface
    Signed-off-by: wangchunyan1 <wangchunyan1@letv.com>

--- scripts/MoniRedisMem/MoniReidsMem.sh
+++ scripts/MoniRedisMem/MoniReidsMem.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
-REDISFILE=/home/wangchunyan/work/go/myowncode/scripts/MoniRedisMem/redis.txt
+PROG_DIR=/home/wangchunyan/work/go/myowncode/scripts/MoniRedisMem
+LOG_DIR=/letv/wangchunyan/CodisMoni
+LOGFILE=$LOG_DIR/redis_memory_monitor.log
+LOGSIZE=104857600
+
+PROG_MAIL=$PROG_DIR/http_mail.py
+TMPMAIL=/tmp/mail.tmp
+
+function log()
+{
+    echo "`date`: $1 $2 $3 $4 $5 $6 $7 $8" >> $LOGFILE
+    # if logfile is too big,backup one
+    filesize=`stat $LOGFILE | grep Size | awk '{print $2}'`
+    if [ $filesize -gt $LOGSIZE ] ; then
+        mv $LOGFILE $LOGFILE".old"
+    fi
+}
+
+function debug()
+{
+#    log $1 $2 $3 $4 $5 $6 $7 $8
+    return
+}
+
+REDISFILE=$PROG_DIR/redis.txt
 REDISCLI=/home/wangchunyan/install/codis/bin/redis-cli
 
 HTML_HEAD="<html><head><title>redis memory monitor(`date +"%x %X"`)</title></head><body><table border='1' cellspacing='0' cellpadding='0' align='center'><tr><th width='200'>Redis</th><th width='150'>Memory</th></tr>"
@@ -12,23 +36,22 @@ ALL_CONTENT=""
 MAIL_ADDR="wangchunyan1@letv.com"
 function send_mail()
 {
+	log "send_mail for monitor redis memory"
 	mailtitle="REDIS内存实时统计(`date +"%x %X"`)"
-	subject="`echo $mailtitle |base64`"
 
 	ALL_CONTENT=$ALL_CONTENT"<tr><td>ALL</td><td>`echo "$ALL_MEM  / 1048576" | bc` M</td></tr>"
-	echo "send monitor data to:$MAIL_ADDR" >> /tmp/redismonitor.txt
-	echo "$ALL_CONTENT" >> /tmp/redismonitor.txt
-	(
-	echo "Subject: =?UTF-8?B?$subject?="
-	echo "MIME-Version: 1.0"
-	echo "Content-Type: text/html;charset=\"UTF-8\""
-	echo "Content-Disposition: inline"
-	echo "To:$MAIL_ADDR"
+	echo "`date` :send monitor data to:$MAIL_ADDR" >> $LOGFILE
+	
+	echo "$HTML_HEAD" >> $LOGFILE
+	echo "$ALL_CONTENT" >> $LOGFILE
+	echo "$HTML_TAIL" >> $LOGFILE
+	
+	echo "$HTML_HEAD" > $TMPMAIL
+	echo "$ALL_CONTENT" >> $TMPMAIL
+	echo "$HTML_TAIL" >> $TMPMAIL
 
-	echo "$HTML_HEAD"
-	echo "$ALL_CONTENT"
-	echo "$HTML_TAIL"
-	) | /usr/sbin/sendmail -t -i $MAIL_ADDR
+	log "$PROG_MAIL push_redis_monitor \"$mailtitle\" $TMPMAIL"	
+	$PROG_MAIL push_redis_monitor "$mailtitle" $TMPMAIL 1>/dev/null 2>/dev/null
 }
 
 TMPFILE=/tmp/.number.swap
@@ -46,12 +69,14 @@ function get_per_redis()
 
 function get_redis_mem()
 {
+	log "begin get_redis_mem"
 	while read line
 	do
 		if [ -n "$line" ] ; then
 			get_per_redis $line
 		fi
 	done < $REDISFILE
+	log "end get_redis_mem"
 }
 
 get_redis_mem
